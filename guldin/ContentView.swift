5ab77d7fa116f392631e3767196bd301bf53cd44
(SangYeop) <(freiheit517@icoud.com)>
2024-05-03 22:23:12 +0900

    Initial Commit

---
+++ guldin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  guldin
+//
+//  Created by 임상엽 on 5/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ guldin/guldinApp.swift
@@ -0,0 +1,17 @@
+//
+//  guldinApp.swift
+//  guldin
+//
+//  Created by 임상엽 on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct guldinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
